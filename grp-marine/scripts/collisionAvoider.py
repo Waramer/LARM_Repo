d5aa1eda7b5a190118bb5ae2794cdaa700f95bc5
BenjaminDuchiron <bduchiron.42@gmail.com>
2022-01-17 17:55:06 +0100

    version_evitement

---
+++ grp-marine/scripts/collisionAvoider.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+from threading import Timer
+import rospy
+import math
+from rospy.core import loginfo
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+move_command = Twist()
+move_command.linear.x = 0.0
+move_command.angular.z = 0.0
+
+def observe(data):
+    obstacles = []
+    angle= data.angle_min
+    for aDistance in data.ranges :
+        if 0.1 < aDistance and aDistance < 1.0 :
+            aPoint= [ 
+                math.cos(angle) * aDistance,    # X
+                math.sin(angle) * aDistance,    # Y
+                aDistance,                      # rho
+                angle                           # theta
+            ]
+            obstacles.append(aPoint)
+        angle+= data.angle_increment
+    avoid_collision(obstacles)
+
+def move(vel):
+    global move_command
+    move_command.linear.x = vel
+
+def stop():
+    global move_command
+    move_command.linear.x = 0.0
+
+def turn(vel):
+    global move_command
+    move_command.angular.z = vel
+
+def zone_coll(point,lat,lon):
+    if -lat<point[1] and point[1]<lat :
+        if point[0] >0 and point[2]<lon :
+            return True
+    return False
+
+def analyse(obstacles):
+    lat_sit = 0
+    min_rng_prox = 1.0
+    min_rng_coll = 1.0
+    nb = 0
+    nb_2 = 0
+
+    for obstacle in obstacles:
+        
+        nb += 1
+        if zone_coll(obstacle,0.2,1.0):  # si obstacles dans le couloir
+            if obstacle[2] < min_rng_coll :
+                min_rng_coll = obstacle[2]
+        if zone_coll(obstacle,0.3,0.5):
+            if obstacle[2] < min_rng_prox:
+                min_rng_prox = obstacle[2]
+        if zone_coll(obstacle,0.3,0.7) :
+            nb_2 += 1
+            if obstacle[1]>0 :
+                lat_sit -= (1-obstacle[2])*(1-obstacle[1])
+            else :
+                lat_sit -= (1-obstacle[2])*(obstacle[1]-1)
+         
+        if str(move_command.linear.x)==0.0 and str(move_command.angular.z) ==0.0:
+        
+            min_rng_coll = -3 
+       
+    if nb_2 != 0 :
+        lat_sit /= nb_2
+
+    
+           
+    #rospy.loginfo(lat_sit)
+    return lat_sit,min_rng_prox,min_rng_coll
+
+
+
+   
+
+def avoid_collision(obstacles):
+    move(0.3)
+    lat_sit,min_r_p,min_r_c= analyse(obstacles)
+    if math.fabs(lat_sit) > 1.0 :
+        turn(lat_sit/math.fabs(lat_sit))
+    elif math.fabs(lat_sit) > 0.01 :
+        turn(2*lat_sit)
+    else :
+        turn(0) 
+
+    if min_r_c < 0.5 :
+        stop()
+    elif min_r_c ==-3 :
+        move(-1)
+    else :
+        move(0.3*min_r_c)
+
+
+    transmit()
+
+def transmit():
+    global move_command
+    pub = rospy.Publisher('cas_cmd', Twist, queue_size=10)
+    pub.publish(move_command)
+    rospy.loginfo(str(move_command.linear.x)+" : "+str(move_command.angular.z))
+
+def main_prog():
+    rospy.init_node('Driver', anonymous=True)
+    rospy.Subscriber("base_scan", LaserScan, observe)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main_prog()
--- grp-marine/scripts/driver.py
+++ grp-marine/scripts/driver.py
@@ -6,8 +6,12 @@ from geometry_msgs.msg import Twist
 
 state = Twist()
 move_command = Twist()
+new_command =Twist()
+new_command.linear.x = 0.0
+new_command.angular.z = 0.0
 lin_rate = 0.05
 ang_rate = 0.07
+decision = False
 
 def accel(vel,rate,cmd):
     if math.fabs(cmd-vel) < 0.01 :
@@ -15,26 +19,43 @@ def accel(vel,rate,cmd):
     else :
         return vel+rate*(cmd-vel)
 
-def update(data):
+def updatenav(data):
     global move_command
     move_command = data
+
+def updatecas(data):
+    global decision
+    global new_command
+    new_command =data
+    if (new_command.linear.x != 0.3 and new_command.angular.z != 0.0) :
+        decision =True
+    else :
+        decision = False
+
 
 def move(data):
     global state
     global move_command
     global lin_rate
     global ang_rate
-
-    state.linear.x = accel(state.linear.x,lin_rate,move_command.linear.x)
-    state.angular.z = accel(state.angular.z,ang_rate,move_command.angular.z)
-
+    global new_command
+    global decision
+    if decision==True:
+        print("lol")
+        state.linear.x = accel(state.linear.x,lin_rate,new_command.linear.x)
+        state.angular.z = accel(state.angular.z,ang_rate,new_command.angular.z)
+    else:
+        state.linear.x = accel(state.linear.x,lin_rate,move_command.linear.x)
+        state.angular.z = accel(state.angular.z,ang_rate,move_command.angular.z)
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     pub.publish(state)
 
+
+
 def main_prog():
     rospy.init_node('Driver', anonymous=True)
-    rospy.Subscriber("nav_cmd", Twist, update)
-    rospy.Subscriber("cas_cmd", Twist, update)
+    rospy.Subscriber("nav_cmd", Twist, updatenav)
+    rospy.Subscriber("cas_cmd", Twist, updatecas)
     rospy.Timer(rospy.Duration(0.05), move)
     rospy.spin()
 
